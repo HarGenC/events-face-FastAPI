f2896b613f49ec7c950f03684364610d70ace18d
HarGenC <politov.valentin@mail.ru>
2026-03-13 12:13:49 +0300

    fixed entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-until pg_isready -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER
+until pg_isready -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USERNAME
 do
   sleep 2
 done
