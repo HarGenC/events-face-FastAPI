db7020f70847431f60d716a74aabf24f9aba05e9
HarGenC <politov.valentin@mail.ru>
2026-03-18 10:45:34 +0300

    change status code for register_for_event

--- app/modules/tickets/controller.py
+++ app/modules/tickets/controller.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter, Depends
+from http import HTTPStatus
+
+from fastapi import APIRouter, Depends, Response
 
 from app.modules.tickets.dependencies import get_ticket_service
 from app.modules.tickets.schemas import RegistrationInfoIn
@@ -7,13 +9,14 @@ from app.modules.tickets.service import TicketService
 router = APIRouter(prefix="/tickets", tags=["tickets"])
 
 
-@router.post("", summary="Get ticket")
+@router.post("", summary="Register and get ticket")
 async def register_for_event(
     registration_info: RegistrationInfoIn,
     ticket_service: TicketService = Depends(get_ticket_service),
 ):
-    ticket_id = await ticket_service.register_for_event(
-        registration_info=registration_info
+    ticket_id = await ticket_service.register_for_event(registration_info)
+    return Response(
+        content={"ticket_id": ticket_id},
+        media_type="application/json",
+        status_code=HTTPStatus.CREATED,
     )
-
-    return {"ticket_id": ticket_id}
